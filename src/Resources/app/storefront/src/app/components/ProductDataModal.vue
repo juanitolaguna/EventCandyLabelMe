d3040768d65da24b331bd41bc2a21791e0a38160
juanitolaguna <juanitolaguna@protonmail.com>
2021-04-15 12:37:09 +0200

    add print option to product data modal

--- src/Resources/app/storefront/src/app/components/ProductDataModal.vue
+++ src/Resources/app/storefront/src/app/components/ProductDataModal.vue
@@ -14,7 +14,7 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body">
+        <div id="printContent" class="modal-body">
           <div v-for="infoBlock in data">
             <div v-html="infoBlock"></div>
             <hr>
@@ -31,7 +31,12 @@
 
           <button v-on:click.prevent="onOk" style="min-width: min-content" type="button"
                   class="btn btn-primary d-none d-sm-block">
-            <span >Schließen</span>
+            <span>Schließen</span>
+          </button>
+
+          <button v-on:click.prevent="onPrint" style="min-width: min-content" type="button"
+                  class="btn btn-primary">
+            <span>Drucken</span>
           </button>
 
         </div>
@@ -49,7 +54,49 @@ export default {
 
   methods: {
     onOk() {
-        this.$emit('close-product-modal');
+      this.$emit('close-product-modal');
+    },
+
+    onPrint() {
+
+      const styles = `
+        body {
+          padding: 3rem;
+        }
+        .noprint {
+          padding: 5px 20px;
+          float: right;
+          background-color: #FA9B00;
+          color: white;
+          border-radius: 6px;
+          border: none;
+          font-size: 1.2em;
+          font-weight: 600;
+          cursor: pointer;
+        }
+        @media print {
+          .noprint {
+            display: none !important;
+          }
+        }`;
+
+
+      const printContent = document.getElementById('printContent');
+
+      const w = window.open('', '_blank', 'height=800,width=600');
+      w.document.write(printContent.innerHTML);
+
+      // add styles
+      const styleSheet = w.document.createElement('style');
+      styleSheet.innerHTML = styles;
+      w.document.head.appendChild(styleSheet);
+
+      // add button
+      const button = document.createElement("div");
+      button.innerHTML = `<button class="noprint" type="button" onclick="window.print(); return false;">Drucken</button><br><br>`;
+      w.document.body.prepend(button);
+      w.document.close();
+      w.focus();
     }
   }
 }
