7829526c2e5425cda428c59e30f4afea76ec976b
juanitolaguna <juanitolaguna@protonmail.com>
2021-10-22 14:52:52 +0200

    Add editable SEO options to plugin config

--- src/Storefront/Controller/LabelMeController.php
+++ src/Storefront/Controller/LabelMeController.php
@@ -2,10 +2,13 @@
 
 namespace EventCandy\LabelMe\Storefront\Controller;
 
+use ErrorException;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Controller\StorefrontController;
 use Shopware\Storefront\Framework\Cache\Annotation\HttpCache;
+use Shopware\Storefront\Page\MetaInformation;
 use Shopware\Storefront\Page\Navigation\NavigationPageLoader;
 use Shopware\Storefront\Pagelet\Menu\Offcanvas\MenuOffcanvasPageletLoaderInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,11 +27,18 @@ class LabelMeController extends StorefrontController
      */
     private $navigationPageLoader;
 
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
 
     public function __construct(
-        NavigationPageLoader $navigationPageLoader
+        NavigationPageLoader $navigationPageLoader,
+        SystemConfigService $systemConfigService
     ) {
         $this->navigationPageLoader = $navigationPageLoader;
+        $this->systemConfigService = $systemConfigService;
     }
 
     /**
@@ -38,6 +48,21 @@ class LabelMeController extends StorefrontController
     {
 
         $page = $this->navigationPageLoader->load($request, $context);
+        $config = $this->systemConfigService->get('EventCandyLabelMe.config');
+
+        /** @var MetaInformation $metaInformation */
+        $metaInformation = $page->getMetaInformation();
+        $metaInformation->setMetaTitle($config['lmSEOMetaTitle']);
+        $metaInformation->setMetaDescription($config['lmSEOMetaDescription']);
+
+        $days = $config['lmSeoRevisitAfter'];
+        if ($days == 0 || $days == null) {
+            $days = 15;
+        }
+        $metaInformation->setRevisit((string) $days . ' days');
+        $metaInformation->setMetaKeywords($config['lmSeoKeywords']);
+        $page->setMetaInformation( $metaInformation);
+
 
         return $this->renderStorefront(
             '@EventCandyLabelMe/storefront/page/eclm.html.twig',
