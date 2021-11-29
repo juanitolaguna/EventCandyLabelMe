62ec0a1e0af1291f2e12f879e87305956b1e44a9
juanitolaguna <juanitolaguna@protonmail.com>
2021-11-29 01:29:39 +0100

    inherit stock updaterfuntions from setproduct (uses same payload)

--- src/Core/LabelMeLineItemStockUpdaterFunctions.php
+++ src/Core/LabelMeLineItemStockUpdaterFunctions.php
@@ -2,53 +2,14 @@
 
 namespace EventCandy\LabelMe\Core;
 
-use EventCandy\LabelMe\Core\Checkout\Cart\EclmCartProcessor;
-use EventCandy\Sets\Core\Content\Product\DataAbstractionLayer\LineItemStockUpdaterFunctionsInterface;
-use Shopware\Core\Checkout\Cart\Event\CheckoutOrderPlacedEvent;
-use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
-use Shopware\Core\Framework\Uuid\Uuid;
+use EventCandy\LabelMe\Core\Checkout\Cart\LabelMeCartCollector;
+use EventCandy\Sets\Core\Content\Product\DataAbstractionLayer\SetProductLineItemStockUpdaterFunctions;
 
-class LabelMeLineItemStockUpdaterFunctions implements LineItemStockUpdaterFunctionsInterface
+class LabelMeLineItemStockUpdaterFunctions extends SetProductLineItemStockUpdaterFunctions
 {
 
     public function getLineItemType(): string
     {
-        return EclmCartProcessor::TYPE;
-    }
-
-    public function createOrderLineItemProducts(OrderLineItemEntity $lineItem, CheckoutOrderPlacedEvent $event): array
-    {
-        // TODO: Implement createOrderLineItemProducts() method.
-
-        $order = $event->getOrder();
-        $lineItemQuantity = $lineItem->getQuantity();
-
-        $orderLineItems = [];
-
-        // Main Product
-        $mainOrderLineItemId = Uuid::randomHex();
-        $orderLineItems[] = [
-            'id' => $mainOrderLineItemId,
-            'productId' => $lineItem->getReferencedId(),
-            'orderId' => $order->getId(),
-            'orderLineItemId' => $lineItem->getId(),
-            'quantity' => $lineItemQuantity
-        ];
-
-
-        $subProducts = $lineItem->getPayload()[$this->getLineItemType()];
-
-        foreach ($subProducts as $product) {
-            $orderLineItems[] = [
-                'id' => Uuid::randomHex(),
-                'parentId' => $mainOrderLineItemId,
-                'productId' => $product['product_id'],
-                'orderId' => $order->getId(),
-                'orderLineItemId' => $lineItem->getId(),
-                'quantity' => $product['quantity'] * $lineItemQuantity
-            ];
-        }
-
-        return $orderLineItems;
+        return LabelMeCartCollector::TYPE;
     }
 }
